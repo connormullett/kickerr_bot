824633b86d1fd748bf5dfc8f75c41f6979524c01
connor mullett <connormullett@gmail.com>
2022-02-14 00:40:50 -0500

    set up deps; CAS request on user join update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+
+use serde::{Deserialize, Serialize};
+use teloxide::{dispatching2::UpdateFilterExt, prelude2::*};
+
+#[derive(Serialize, Deserialize)]
+struct CasResponse {
+    ok: bool,
+    description: Option<String>,
+    result: Option<String>,
+}
+
+#[tokio::main]
+async fn main() {
+    run().await
+}
+
+async fn run() {
+    teloxide::enable_logging!();
+
+    dotenv().ok();
+
+    let bot = Bot::from_env().auto_send();
+
+    let handler = Update::filter_message().branch(dptree::endpoint(
+        |msg: Message, bot: AutoSend<Bot>| async move {
+            if let Some(users) = msg.new_chat_members() {
+                for user in users {
+                    let result: CasResponse =
+                        reqwest::get(format!("https://api.cas.chat/check?user_id={}", user.id))
+                            .await
+                            .unwrap()
+                            .json()
+                            .await
+                            .unwrap();
+                }
+            }
+            respond(())
+        },
+    ));
+
+    Dispatcher::builder(bot, handler)
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch()
+        .await;
 }
